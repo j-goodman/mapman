0e7a1525cf2d67a9fa786c59aa63927b32eed62a
John Goodman <johncurtisgoodman@gmail.com>
2017-05-31 12:48:21 -0400

    Remove all display overlays

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var map;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 34.397, lng: 250.644},
+    disableDefaultUI: true,
     zoom: 11,
   });
-  console.log(map);
+  map.mapTypeId = 'satellite';
+  // console.log(Object.getOwnPropertyNames(map));
 }
+
+window.onload = function () {
+  var buttons;
+  var i;
+  buttons = document.getElementsByClassName('gm-style-cc');
+  for (i=0 ; i<buttons.length ; i++) {
+    buttons[i].style.display = 'none';
+  }
+  buttons = document.getElementsByTagName('img');
+  buttons[13].style.display = 'none';
+};
